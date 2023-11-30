b42b105fc98177a3a24c66cee23102a6b1490db7
Domenica Escobar <domenica.escobar.moyano@udla.edu.ec>
2023-11-29 21:13:27 -0600

    Completo

--- Models/About.cs
+++ Models/About.cs
@@ -10,7 +10,7 @@ namespace Escobar_AppApuntes.Models
     {
         public string Title => AppInfo.Name;
         public string Version => AppInfo.VersionString;
-        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string MoreInfoUrl => "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
         public string Message => "This app is written in XAML and C# with .NET MAUI.";
     }
 }
